4ff6a4adb746c895f0d011062767c88c628425b6
Fred Morris <consulting@m3047.net>
2019-06-27 09:49:31 -0700

    Remove reference to agents.

--- app/configuration_sample.py
+++ app/configuration_sample.py
@@ -8,6 +8,5 @@ REDIS_SERVER = 'localhost'
 # sensitivity. DNS is not supposed to be case sensitive.
 #
 # If you want to use hostnames and DNS, then you may want to set USE_DNSPTYHON
-# to True. Of course, dnspython has to be installed. This isn't an issue with
-# the DNS agent but the PCAP agent has no intrinsic dependency on it.
+# to True. Of course, dnspython has to be installed.
 USE_DNSPYTHON = False
