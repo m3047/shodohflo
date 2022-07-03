1f3474995ec05ad71c230bb1c33a6eb0f2461f17
Fred Morris <consulting@m3047.net>
2022-07-02 18:47:01 -0700

    Warn when we drop one of these broken packets.

--- agents/pcap_agent.py
+++ agents/pcap_agent.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Copyright (c) 2019 by Fred Morris Tacoma WA
+# Copyright (c) 2019,2022 by Fred Morris Tacoma WA
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -316,6 +316,7 @@ class Server(object):
                     # only normal cases we care about (but we care about them both).
                     if   dst in self.our_network:
                         if   pkt.p == socket.IPPROTO_TCP and not isinstance(pkt.data, dpkt.tcp.TCP):
+                            logging.warn('TCP packet cannot be decoded {}->{}'.format(src, dst))
                             # Reject TCP packets which cannot be decoded.
                             break
                         elif pkt.p == socket.IPPROTO_TCP and pkt.data.flags & dpkt.tcp.TH_RST:
