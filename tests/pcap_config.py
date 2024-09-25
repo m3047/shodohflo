17ae9158606e898684a6bb880ca9ca4db0b26237
Fred Morris <consulting@m3047.net>
2024-09-25 16:03:14 -0700

    Tests had the wrong license.

--- tests/pcap_config.py
+++ tests/pcap_config.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 # Copyright (c) 2024 Fred Morris Tacoma WA USA
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License version 3,
-# as published by the Free Software Foundation.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """Tests for ../shodohflo/pcap_config.py
 
